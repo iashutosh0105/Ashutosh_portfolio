55d60bddfa2d84edad1ddc9e63785a68f8d45003
Ashutosh Kumar <ashutoshkumar@accuratics.com>
2025-07-07 05:21:10 +0530

    initial commit

--- Ashutosh-Portfolio-main/src/components/Footer.jsx
+++ Ashutosh-Portfolio-main/src/components/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/huzaifahmedz/"
+        href="https://www.instagram.com/ft.asuuu_?igsh=dmVocG5sdmk3djRo"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
+      {/* <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
         <FaXTwitter size={24} />
-      </a>
+      </a> */}
       <a
-        href="https://www.linkedin.com/in/huzaif-ahmed-6abb49273/"
+        href="https://www.linkedin.com/in/ashutosh-kumar-873179276?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
         target="_blank"
         rel="noopener noreferrer"
       >
